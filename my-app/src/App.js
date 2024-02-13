39db15ed9fb6c1c4123daa264fcc3d0397a04b7e
Matt Shapland <matt.shapland@buildertrend.com>
2024-02-13 14:03:15 -0600

    started messing with antd

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Pokédex</h1>
-    </div>
-  );
-}
-
+import React from 'react';
+import { Button, Flex } from 'antd';
+const App = () => (
+  <Flex gap="small" wrap="wrap">
+    <Button type="primary">Primary Button</Button>
+    <Button>Default Button</Button>
+    <Button type="dashed">Dashed Button</Button>
+    <Button type="text">Text Button</Button>
+    <Button type="link">Link Button</Button>
+  </Flex>
+);
 export default App;
