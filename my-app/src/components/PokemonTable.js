0f2e59d61e117e504fbfc8df4a36844094154276
Matt Shapland <matt.shapland@buildertrend.com>
2024-02-16 15:16:15 -0600

    got modal working

--- my-app/src/components/PokemonTable.js
+++ my-app/src/components/PokemonTable.js
@@ -1,6 +1,9 @@
 import React from "react";
-import { Space, Table, Tag } from "antd";
+import { Table, Typography, Avatar, Modal } from "antd";
 import Spinner from "./Spinner";
+import { useState } from "react";
+
+const { Title } = Typography;
 
 const columns = [
     {
@@ -9,17 +12,70 @@ const columns = [
         key: "number",
     },
     {
+        title: "Image",
+        dataIndex: "id",
+        key: "image",
+        render: (id) => {
+            return (
+                <Avatar
+                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`}
+                    size={"large"}
+                />
+            );
+        },
+    },
+    {
         title: "Name",
         dataIndex: "name",
         key: "name",
-        render: (name) => <a>{name.english}</a>,
+        render: (name) => <Title level={4}>{name.english}</Title>,
+    },
+    {
+        title: "Type",
+        dataIndex: "type",
+        key: "type",
+        render: (type) => <div>{type.join(", ")}</div>,
     },
 ];
 
 function PokemonTable(props) {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
     //if we have pokemon data, return our table
     if (props.pokemon !== null) {
-        return <Table columns={columns} dataSource={props.pokemon} />;
+        return (
+            <div>
+                <Table
+                    columns={columns}
+                    dataSource={props.pokemon}
+                    onRow={(record) => {
+                        return {
+                            onClick: () => {
+                                showModal();
+                            },
+                        };
+                    }}
+                />
+                <Modal
+                    title="Basic Modal"
+                    open={isModalOpen}
+                    onOk={handleOk}
+                    onCancel={handleCancel}
+                >
+                    <p>Some contents...</p>
+                    <p>Some contents...</p>
+                    <p>Some contents...</p>
+                </Modal>
+            </div>
+        );
     }
     //if we don't have pokemon data, return the spinner
     else {
