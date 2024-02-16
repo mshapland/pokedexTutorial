22ef4a828d91b3eda27fb0085060e85747bdb79a
Matt Shapland <matt.shapland@buildertrend.com>
2024-02-16 14:11:55 -0600

    applied prettier to the solution

--- my-app/src/components/LandingPage.js
+++ my-app/src/components/LandingPage.js
@@ -1,22 +1,21 @@
-import api from '../api/api';
-import CountButton from './CountButton';
+import api from "../api/api";
+import CountButton from "./CountButton";
 
 //Our Landing Page
 function LandingPage() {
+    //call our backend to get pokemon data
+    let data = api
+        .get("/pokemon")
+        .then(function (response) {
+            // handle success
+            console.log(response);
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        });
 
-  //call our backend to get pokemon data
-  let data = api.get('/pokemon').then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  
-  return (
-    <CountButton />
-  );
+    return <CountButton />;
 }
 
 export default LandingPage;
